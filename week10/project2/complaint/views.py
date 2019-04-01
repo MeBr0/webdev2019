2e99c112d1aff551685860bbd04bfceb08112a75
mebr0 <aza17e@gmail.com>
2019-04-01 23:18:54 +0600

    comments of single complaint

--- week10/project2/complaint/views.py
+++ week10/project2/complaint/views.py
@@ -1,30 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Complaint, Comment
+from complaint.models import Complaint, Comment
 
 
 def complaints(request):
-    return HttpResponse(Complaint.objects.all())
+    if request.method == 'GET':
+        return HttpResponse(Complaint.objects.all())
+
+    elif request.method == 'POST':
+        return HttpResponse(request.data)
 
 
 def complaint_by_id(request, complaint_id):
-    try:
-        res = Complaint.objects.get(pk=complaint_id)
+    if request.method == 'GET':
+        try:
+            res = Complaint.objects.get(pk=complaint_id)
 
-        print(res)
+            print(res)
+
+            return HttpResponse(res)
+
+        except:
+            return HttpResponse('Complaint with id: %d not found' % complaint_id)
+
+    elif request.method == 'POST':
+        return HttpResponse(request.data)
+
+
+def comments(request, complaint_id):
+    try:
+        res = Comment.objects.get(complaint=complaint_id)\
 
         return HttpResponse(res)
 
     except:
-        return HttpResponse('Complaint with id: %d not found' % complaint_id)
-
-def comments(request, complaint_id):
-    try:
-        res = Complaint.objects.filter()
-        pass
-
-    except:
-        pass
+        return HttpResponse('not found!')
 
 
