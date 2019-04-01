5a04feb2c1e077d036b48be0e39e7adbe68c9109
mebr0 <aza17e@gmail.com>
2019-04-01 18:34:59 +0600

    env renamed?

--- week10/project1/env/bin/django-admin.py
+++ week10/project1/env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/mebr0/Documents/KBTU/4 semester/Web Development/Repository/week10/project1/myenv/bin/python3
+#!/home/mebr0/Documents/KBTU/4 semester/Web Development/Repository/week10/project1/env/bin/python3
 from django.core import management
 
 if __name__ == "__main__":
