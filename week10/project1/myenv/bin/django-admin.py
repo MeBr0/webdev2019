46abc450d09fd883d63417326ee555f7a704c44a
mebr0 <aza17e@gmail.com>
2019-04-01 17:32:46 +0600

    first project on Django

---
+++ week10/project1/myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mebr0/Documents/KBTU/4 semester/Web Development/Repository/week10/project1/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
